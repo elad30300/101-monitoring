9deea12b7b8448ded9ddb12e87bc66f6031a1cfd
elad3030301 <elad3030301@gmail.com>
2020-04-12 13:58:58 +0300

    Makes user to enable bluetooth at the beginning of the app (only if not enabled already)

--- app/src/main/java/com/example/a101_monitoring/MainActivity.kt
+++ app/src/main/java/com/example/a101_monitoring/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.a101_monitoring
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -23,7 +26,9 @@ class MainActivity : AppCompatActivity(), PatientsListFragment.OnListFragmentInt
 
 //        (applicationContext as MyApplication).applicationComponent.inject(this)
 
+        initializePermissions()
 
+        checkIfBluetoothOn()
     }
 
     override fun onListFragmentInteraction(item: Patient?) {
@@ -31,6 +36,34 @@ class MainActivity : AppCompatActivity(), PatientsListFragment.OnListFragmentInt
             val action = PatientsListFragmentDirections.actionPatientFragmentToPatientManualMeasurmentsFragment(id)
             nav_host_fragment.findNavController().navigate(action) // TODO check if this is correct implementation
         }
+    }
+
+    /* check if BT is on/off
+     * on - do nothing
+      off - popup to open BT*/
+    private fun checkIfBluetoothOn() {
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter == null) {
+            // Phone doesn't support BT
+            Toast.makeText(this, "BT not supported exit app", Toast.LENGTH_SHORT).show()
+            this.finishAndRemoveTask()
+        } else if (!bluetoothAdapter.isEnabled) {
+            // BT is disabled and need to be open
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+    }
+
+    private fun initializePermissions() {
+        val PERMISSIONS_STORAGE = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        requestPermissions(
+            PERMISSIONS_STORAGE,
+            1234)
     }
 
 //    private fun setStatesObservers() {
@@ -43,4 +76,7 @@ class MainActivity : AppCompatActivity(), PatientsListFragment.OnListFragmentInt
 //        })
 //    }
 
+    companion object {
+        private const val REQUEST_ENABLE_BT = 1
+    }
 }
